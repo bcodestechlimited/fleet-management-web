712e5d9b18416ff9727889f448e006469d3fc1f0
Emmanuel Eguavoen <eguavoenemmanuel2019@gmail.com>
2025-04-11 09:21:24 +0100

    fix: changed input type from text to email

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
               Email Address
             </label>
             <input
-              type="text"
+              type="email"
               className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500  "
             />
           </div>
